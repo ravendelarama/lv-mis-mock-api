38dd92213cca638697f67a6ad67ffaede6deb6e5
Kertsu <kurtddbigtas@gmail.com>
2024-09-29 14:04:05 +0800

    add studentSchoolId field

--- src/utils/seeder.ts
+++ src/utils/seeder.ts
@@ -1,45 +1,51 @@
 import { faker } from "@faker-js/faker";
 
 export const seed = {
-    student: async (count: number) => {
-        const seeds = Array();
+  student: async (count: number) => {
+    const seeds = Array();
 
-        for (count; count > 0; count--) {
-            const firstName = faker.person.firstName();
-            const middleName = faker.person.middleName();
-            const lastName = faker.person.lastName();
+    for (let i = 1; i <= count; i++) {
+      const firstName = faker.person.firstName();
+      const middleName = faker.person.middleName();
+      const lastName = faker.person.lastName();
 
-            const newStudent = {
-                firstName,
-                middleName,
-                lastName,
-                email: `${firstName.toLowerCase()}${lastName.toLowerCase()}@student.laverdad.edu.ph`,
-                irregular: false
-            };
+      const studentSchoolId = `21-${String(i).padStart(
+        5,
+        "0"
+      )}${faker.string.alpha({ length: 3, casing: "upper" })}`;
 
-            seeds.push(newStudent)
-        }
+      const newStudent = {
+        firstName,
+        middleName,
+        lastName,
+        studentSchoolId,
+        email: `${firstName.toLowerCase()}${lastName.toLowerCase()}@student.laverdad.edu.ph`,
+        irregular: false,
+      };
 
-        return [...seeds]
-    },
-    instructor: async (count: number) => {
-        const seeds = Array();
-
-        for (count; count > 0; count--) {
-            const firstName = faker.person.firstName();
-            const middleName = faker.person.middleName();
-            const lastName = faker.person.lastName();
-
-            const newInstructor = {
-                firstName,
-                middleName,
-                lastName,
-                email: `${firstName.toLowerCase()}${lastName.toLowerCase()}@laverdad.edu.ph`,
-            };
-
-            seeds.push(newInstructor)
-        }
-
-        return [...seeds]
+      seeds.push(newStudent);
     }
-}
+
+    return [...seeds];
+  },
+  instructor: async (count: number) => {
+    const seeds = Array();
+
+    for (count; count > 0; count--) {
+      const firstName = faker.person.firstName();
+      const middleName = faker.person.middleName();
+      const lastName = faker.person.lastName();
+
+      const newInstructor = {
+        firstName,
+        middleName,
+        lastName,
+        email: `${firstName.toLowerCase()}${lastName.toLowerCase()}@laverdad.edu.ph`,
+      };
+
+      seeds.push(newInstructor);
+    }
+
+    return [...seeds];
+  },
+};

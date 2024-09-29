57977c462e9a456d896b4e6dde6418e5b0b06d13
Kertsu <kurtddbigtas@gmail.com>
2024-09-29 14:29:44 +0800

    add get student by id and id type

--- src/routes/college-student.ts
+++ src/routes/college-student.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
-import { getCollegeStudents, seedCollegeStudents } from "../controllers";
+import {
+  getCollegeStudentByIdAndIdType,
+  getCollegeStudents,
+  seedCollegeStudents,
+} from "../controllers";
 
 const router = Router();
 
-
-router.get('/students', getCollegeStudents);
-router.get('/students/seed', seedCollegeStudents);
-
+router.get("/students", getCollegeStudents);
+router.get("/students/:idType/:id", getCollegeStudentByIdAndIdType);
+router.get("/students/seed", seedCollegeStudents);
 
 export default router;

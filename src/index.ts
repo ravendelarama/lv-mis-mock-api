9d0a2067b3e438c4bb449da2779d24375bdfc5fc
Kertsu <kurtddbigtas@gmail.com>
2024-10-02 17:43:39 +0800

    add studentSubject

--- src/index.ts
+++ src/index.ts
@@ -6,30 +6,60 @@ import express from "express";
 import http from "http";
 import { awakeServer } from "./jobs";
 import { collegeStudentRouter } from "./routes";
+import { db } from "./models";
 
 dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
 
-app.use(cors({
+app.use(
+  cors({
     origin: [process.env.APP_DEV_URL!, process.env.APP_PROD_URL!],
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 app.use(cookieParser());
-app.use(express.json())
-app.use(express.urlencoded({
-    extended: true
-}))
-app.use(compression())
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(compression());
 
 // routers
-app.use('/college', collegeStudentRouter);
+app.use("/college", collegeStudentRouter);
 
 // to prevent render hosting server termination
-app.get('/', (req, res) => { res.send("Hello Server!") });
+app.get("/", (req, res) => {
+  res.send("Hello Server!");
+});
+
+app.get("/try", async (req, res) => {
+  const studentsTakingSubjectInSection = await db.collegeStudent.findMany({
+    where: {
+      studentSections: {
+        some: {
+          sectionId: "sectionIdHere", // Replace with the actual section ID
+        },
+      },
+      studentSubjects: {
+        some: {
+          subjectId: "subjectIdHere", // Replace with the actual subject ID
+        },
+      },
+    },
+    include: {
+      studentSections: true, // To include the section details in the result
+      studentSubjects: true, // To include the subject details in the result
+    },
+  });
+});
 awakeServer();
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => console.log(`Server is now listening to port ${PORT}.`));
+server.listen(PORT, () =>
+  console.log(`Server is now listening to port ${PORT}.`)
+);

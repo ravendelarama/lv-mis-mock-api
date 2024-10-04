9f9e9fd6ef685823fd3e77c8c6b1e4b44d97d14b
Kertsu <kurtddbigtas@gmail.com>
2024-10-04 11:52:22 +0800

    chore: add get college students by subject ID

--- src/routes/college-subject.ts
+++ src/routes/college-subject.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { getSubjectById, getSubjects } from "../controllers";
+import { getCollegeStudentsBySubjectId, getSubjectById, getSubjects } from "../controllers";
 
 const router = Router();
 
 router.get("/subjects", getSubjects);
 
 router.get("/subjects/:subjectId", getSubjectById);
+
+router.get('/subjects/:subjectId/students', getCollegeStudentsBySubjectId)
 
 export default router;

e053406f965c347b59ab1e11e9b537521fafbf43
Kertsu <kurtddbigtas@gmail.com>
2024-11-06 17:49:54 +0800

    init client side

--- dist/jobs/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.awakeServer = void 0;
-var render_1 = require("./render");
-Object.defineProperty(exports, "awakeServer", { enumerable: true, get: function () { return render_1.awakeServer; } });

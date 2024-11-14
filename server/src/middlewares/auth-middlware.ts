64432f3f1e0680a0c978a2f739c9b8dcdeb440e1
Kertsu <kurtddbigtas@gmail.com>
2024-11-14 13:46:40 +0800

    chore: add get self fn

--- server/src/middlewares/auth-middlware.ts
+++ server/src/middlewares/auth-middlware.ts
@@ -3,26 +3,41 @@ import expressAsyncHandler from "express-async-handler";
 import { NextFunction, Request, Response } from "express";
 import environment from "../constants/environment";
 import { response } from "../utils/response";
-import { db } from "src/models";
+import { db } from "../models";
+
+interface DecodedToken {
+  id: string;
+  iat: number;
+  exp: number;
+}
 
 export const isAuthenticated = expressAsyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
-    let token: string = req.cookies["auth_token"];
+    let token: string | undefined;
 
-    try {
-      const decoded = jwt.verify(
-        token,
-        environment.accessTokenSecret as string
-      );
-      console.log(decoded);
-      next();
-    } catch (err) {
-      console.error("Error verifying token:", err);
-      return response(res, 403, false, "Invalid or expired token", null);
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+        try {
+            token = req.headers.authorization.split(' ')[1];
+            const decoded = jwt.verify(token, environment.accessTokenSecret as string) as DecodedToken
+            console.log(decoded)
+            const authUser = await db.users.findFirst({
+              where: {
+                id: decoded.id,
+              }
+            })
+
+            if (authUser){
+              req.user = authUser
+            }
+            next()
+        } catch (err) {
+            console.error("Error verifying token:", err)
+            return response(res, 403, false, "Invalid or expired token")
+        }
     }
 
     if (!token) {
-      return response(res, 401, false, "Token is not provided", null);
+       return response(res, 401, false, "Token is not provided")
     }
   }
 );
